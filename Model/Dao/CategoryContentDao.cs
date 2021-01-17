bd050ead2b8af4b391674d417df1390e18cf020b
vovuong <vovuong1025@gmail.com>
2021-01-17 16:47:16 +0700

    add Manage CategoryContent for Admin with function Create and PassList

--- Model/Dao/CategoryContentDao.cs
+++ Model/Dao/CategoryContentDao.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Model.EF;
+using PagedList;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Model.EF;
 
 namespace Model.Dao
 {
+    /// <summary>
+    /// The main <c>CategoryContentDao</c> class.
+    /// Contains all methods for performing Category Content query functions
+    /// Author: VoXuanQuocVuong
+    /// Email: vovuong1025@gmail.com
+    /// </summary>
     public class CategoryContentDao
     {
         private TravelAgencyDbContext db = null;
@@ -16,9 +20,63 @@ namespace Model.Dao
             db = new TravelAgencyDbContext();
         }
 
+        /// <summary>
+        /// Query all fields of table CategoryContent and return the List<CONTENTCATEGORY> result
+        /// </summary>
+        /// <returns>A List<CONTENTCATEGORY></returns>
         public List<CONTENTCATEGORY> ListAll()
         {
             return db.CONTENTCATEGORies.ToList();
+        }
+
+        /// <summary>
+        /// Check Exist category content name and return true if already exist of false
+        /// </summary>
+        /// <param name="categoryContentName">A tring category Content Name</param>
+        /// <returns>A bool true if already exist or false</returns>
+        public bool CheckExist(string categoryContentName)
+        {
+            return db.CONTENTCATEGORies.Count(x => x.ContentCategoryName == categoryContentName) > 0;
+        }
+
+        /// <summary>
+        /// Query fields in table CONTENTCATEGORies with ContentCategoryName equal to param searchString
+        /// </summary>
+        /// <param name="searchString">A string to search</param>
+        /// <returns>A IQueryable<CONTENTCATEGORY></returns>
+        public IQueryable<CONTENTCATEGORY> SearchString(string searchString)
+        {
+            return db.CONTENTCATEGORies.Where(x => x.ContentCategoryName.Contains(searchString));
+        }
+
+        /// <summary>
+        /// Query a listPading <CONTENTCATEGORY> with a param is pageStart and a param is pageSize.
+        /// if the param searchString is other null, it is query ContentCategoryName equal searchString.
+        /// </summary>
+        /// <param name="searchString">A string searchString keyword</param>
+        /// <param name="pageNumber">A int page Start</param>
+        /// <param name="pageSize">A int Size pageList</param>
+        /// <returns>A IEnumerable<CONTENTCATEGORY> with ASC</returns>
+        public IEnumerable<CONTENTCATEGORY> ListPading(string searchString, int pageNumber, int pageSize)
+        {
+            IQueryable<CONTENTCATEGORY> model = db.CONTENTCATEGORies;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = SearchString(searchString);
+            }
+            return model.OrderBy(x => x.ContentCategoryName).ToPagedList(pageNumber, pageSize);
+        }
+
+        /// <summary>
+        /// add entity of CONTENTCATEGORY to database and return this id
+        /// </summary>
+        /// <param name="entity">A entity of CONTENTCATEGORY</param>
+        /// <returns>The IDCategory of this CONTENTCATEGORY</returns>
+        public int Create(CONTENTCATEGORY entity)
+        {
+            db.CONTENTCATEGORies.Add(entity);
+            db.SaveChanges();
+            return entity.IDContentCategory;
         }
     }
 }
---
+++ TravelAgency/Areas/Admin/Controllers/CategoryContentController.cs
@@ -0,0 +1,49 @@
+﻿using Model.Dao;
+using Model.EF;
+using System.Web.Mvc;
+
+namespace TravelAgency.Areas.Admin.Controllers
+{
+    // Author: VoXuanQuocVuong
+    // Email: vovuong1025@gmail.com
+    public class CategoryContentController : BaseController
+    {
+        // GET: Admin/CategoryContent
+        public ActionResult Index(string searchString, int pageNumber = 1, int pageSize = 5)
+        {
+            var dao = new CategoryContentDao();
+            var model = dao.ListPading(searchString, pageNumber, pageSize);
+            ViewBag.SeachString = searchString;
+            return View(model);
+        }
+
+        // GET: Admin/CategoryContent/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: Admin/CategoryContent/Create
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(CONTENTCATEGORY model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new CategoryContentDao();
+                if (dao.CheckExist(model.ContentCategoryName))
+                {
+                    ModelState.AddModelError("", "Tên danh mục tour đã tồn tại");
+                }
+                else
+                {
+                    dao.Create(model);
+                    SetAlert("Thêm mới danh mục bài viết thành công", "success");
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+    }
+}
