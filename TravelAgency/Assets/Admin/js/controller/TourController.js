e71c259faeca7dab85b7192c152f8589dfb671fb
vovuong <vovuong1025@gmail.com>
2021-01-19 00:27:56 +0700

    fix delete Tour for Admin use Ajax

---
+++ TravelAgency/Assets/Admin/js/controller/TourController.js
@@ -0,0 +1,38 @@
+﻿var tour = {
+    init: function () {
+        tour.registerEvents();
+    },
+    registerEvents: function () {
+        $('.btn-dlTour').off('click').on('click', function (e) {
+            e.preventDefault();
+            var btn = $(this);
+            var id = btn.data('id');
+            var item = '#row_' + id;
+            $.ajax({
+                url: "/Admin/Tour/Delete",
+                data: { id: id },
+                dataType: "json",
+                type: "POST",
+                success: function (response) {
+                    if (response.status == true) {
+                        $(item).remove();
+                        $('#AlertBox').removeClass('alert-danger');
+                        $('#AlertBox').addClass('alert-success');
+                        $('#AlertBox').slideDown();
+                        $('#AlertBox').delay(1000).slideUp(500);
+                        $('#AlertBox').text("Xóa thành công.");
+                    }
+                    else {
+                        $('#AlertBox').removeClass('alert-success');
+                        $('#AlertBox').addClass('alert-danger');
+                        $('#AlertBox').slideDown();
+                        $('#AlertBox').delay(1000).slideUp(500);
+                        $('#AlertBox').text("Xóa thất bại!");
+                    }
+                }
+            });
+        });
+    }
+}
+
+tour.init();
