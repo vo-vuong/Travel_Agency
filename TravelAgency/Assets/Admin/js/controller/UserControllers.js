97e0c6652bbe8ce9cf755d838610d7ef30c658e9
vovuong <vovuong1025@gmail.com>
2021-01-19 17:54:13 +0700

    fix delete User for Admin use Ajax

---
+++ TravelAgency/Assets/Admin/js/controller/UserControllers.js
@@ -0,0 +1,42 @@
+﻿// Author: VoXuanQuocVuong
+// Email:  vovuong1025@gmail.com
+// Date Create: 19/01/2021
+
+var user = {
+    init: function () {
+        user.registerEvents();
+    },
+    registerEvents: function () {
+        $('.btn-dlUser').off('click').on('click', function (e) {
+            e.preventDefault();
+            var btn = $(this);
+            var id = btn.data('id');
+            var item = '#row_' + id;
+            $.ajax({
+                url: "/Admin/User/Delete",
+                data: { id: id },
+                dataType: "json",
+                type: "POST",
+                success: function (response) {
+                    if (response.status == true) {
+                        $(item).remove();
+                        $('#AlertBox').removeClass('alert-danger');
+                        $('#AlertBox').addClass('alert-success');
+                        $('#AlertBox').slideDown();
+                        $('#AlertBox').delay(1000).slideUp(500);
+                        $('#AlertBox').text("Xóa thành công.");
+                    }
+                    else {
+                        $('#AlertBox').removeClass('alert-success');
+                        $('#AlertBox').addClass('alert-danger');
+                        $('#AlertBox').slideDown();
+                        $('#AlertBox').delay(1000).slideUp(500);
+                        $('#AlertBox').text("Xóa thất bại!");
+                    }
+                }
+            });
+        });
+    }
+}
+
+user.init();
