5056173ec6784364011ab66b3bef2f38b291b1dd
vovuong <vovuong1025@gmail.com>
2023-08-29 21:31:57 +0700

    feat: Add seed for account table and usergroup table

--- Model/Migrations/Configuration.cs
+++ Model/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
 ﻿namespace Model.Migrations
 {
+    using CommonModel.Common;
+    using Model.EF;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -19,6 +21,40 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            context.USERGROUPs.AddOrUpdate(
+                new USERGROUP
+                {
+                    IDUserGroup = "ADMIN",
+                    GroupName = "ADMIN"
+                },
+                new USERGROUP
+                {
+                    IDUserGroup = "MEMBER",
+                    GroupName = "MEMBER"
+                });
+
+            context.ACCOUNTs.AddOrUpdate(
+                new ACCOUNT
+                {
+                    UserName = "admin",
+                    Password = Encryptor.MD5Hash("12345678"),
+                    Name = "admin",
+                    PhoneNumber = "0977816676",
+                    BirthDay = DateTime.Now,
+                    Status = true,
+                    IDUserGroup = "ADMIN"
+                },
+                new ACCOUNT
+                {
+                    UserName = "member",
+                    Password = Encryptor.MD5Hash("12345678"),
+                    Name = "member",
+                    PhoneNumber = "0977816676",
+                    BirthDay = DateTime.Now,
+                    Status = true,
+                    IDUserGroup = "MEMBER"
+                }
+            );
         }
     }
 }
